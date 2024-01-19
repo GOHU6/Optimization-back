b42e76c988f82b33f95226e287d402601a89171b
GOHU6 <oguhom@outlook.fr>
2024-01-19 10:13:57 +0100

    centralize plaintext

--- optimisation_back/src/main/java/com/group/benchmark/TestBM.java
+++ optimisation_back/src/main/java/com/group/benchmark/TestBM.java
@@ -8,10 +8,14 @@ import com.group.CaesarCipher;
 import com.group.RSACipher;
 import com.group.VigenereCipher;
 
+
+
 public class TestBM {
     // Mode.AverageTime | Mode.Throughput | Mode.SingleShotTime | Mode.SampleTime 
     // temps moyen d’exécution | vitesse moyenne d’exécution | @Measurement time et @Warmup ignoré | temps moyens sur des échantillons d’invocations
-    
+
+    private static String plaintext = "SECURITYGOGO";
+
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @Fork(value = 1)
@@ -20,8 +24,7 @@ public class TestBM {
     public void caesarCipherBM() {
         CaesarCipher caesarCipher = new CaesarCipher();
         
-        String plainText = "SECURITYGOGO";
-        caesarCipher.caesarCipherResult(plainText, false);
+        caesarCipher.caesarCipherResult(plaintext, false);
     }
 
     @Benchmark
@@ -31,10 +34,9 @@ public class TestBM {
     @Measurement(iterations = 20, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
     public void vigenereCipherBM() {
         VigenereCipher vigenereCipher = new VigenereCipher();
-        String plainText = "SECURITYGOGO";
         String key = "MAIHY";
 
-        vigenereCipher.vigenereCipherResult(plainText,key,false);
+        vigenereCipher.vigenereCipherResult(plaintext,key,false);
     }
 
     @Benchmark
@@ -44,9 +46,8 @@ public class TestBM {
     @Measurement(iterations = 20, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
     public void rsaCipherBM() throws Exception {
         RSACipher rsaCipher = new RSACipher();
-        String plainText = "SECURITYGOGO";
 
-        rsaCipher.rsaCipherResult(plainText,false);
+        rsaCipher.rsaCipherResult(plaintext,false);
     }
 
 
